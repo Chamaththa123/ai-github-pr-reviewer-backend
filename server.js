53c77f15fba8291a0b4f802090158de97a313476
Chamaththa123 <shamodchamaththa@gmail.com>
2025-08-22 12:10:28 +0530

    create data base conenction

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

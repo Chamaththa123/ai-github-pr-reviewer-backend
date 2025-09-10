1280ab292b3794ee8c5f522d3d534ad12d612134
Chamaththa123 <shamodchamaththa@gmail.com>
2025-09-10 14:43:01 +0530

    get pr details accoding to the repo

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,11 +2,16 @@ const Review = require("../models/Review");
 
 const getReviews = async (req, res) => {
   try {
-    const reviews = await Review.find().sort({ createdAt: -1 });
+    console.log('test')
+    const { id } = req.params;
+    console.log(id)
+    const reviews = await Review.find({ repoId: id }).sort({ createdAt: -1 });
     res.json(reviews);
   } catch (err) {
+    console.error(err)
     res.status(500).json({ message: "Error fetching reviews" });
   }
 };
+
 
 module.exports = { getReviews };
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getReviews } = require("../controllers/reviewController");
 const router = express.Router();
 
-router.get("/", getReviews);
+router.get("/:id", getReviews);
 
 module.exports = router;

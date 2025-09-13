d2cba3cd899f5b5c3cb0805bcb6e31fccdf3b2cb
Chamaththa123 <shamodchamaththa@gmail.com>
2025-09-13 22:27:49 +0530

    Update reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,9 +2,7 @@ const Review = require("../models/Review");
 
 const getReviews = async (req, res) => {
   try {
-    console.log('test')
     const { id } = req.params;
-    console.log(id)
     const reviews = await Review.find({ repoId: id }).sort({ createdAt: -1 });
     res.json(reviews);
   } catch (err) {
